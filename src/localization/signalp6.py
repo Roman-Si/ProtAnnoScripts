937b6836c9c0b26524e51356af81a4a2307b1391
Roman-Si <roman_siap@hotmail.com>
2023-10-25 13:51:14 +0300

    changed signalp6 parsing to work for non-euakryotes

--- src/localization/signalp6.py
+++ src/localization/signalp6.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import os
-import sys
+
 
 
 def signalp6(signalp6_output_file):
@@ -13,12 +12,12 @@ def signalp6(signalp6_output_file):
     
     Returns:
     pandas.DataFrame: DataFrame containing protein IDs, a column indicating the presence of signal peptides and a column with the cleavage positions.
-    """ 
+    """
     df = pd.read_csv(signalp6_output_file, sep = '\t', dtype={'# ID': object}, skiprows=1)
-    df = df.loc[df['Prediction'] == 'SP']
+    df = df.loc[df['Prediction'] != 'OTHER']
     df['proteinId'] = df['# ID'].str.split(' ').str[0]
-    df['SP'] = 'Y'
+    df['SignalP6'] = df['Prediction']
     # Now take CS positions
     # In case of low SP probability there is no CS position given and the column is empty
     df['SP_cs'] = df['CS Position'].apply(lambda x: x.split(': ')[1].split('.')[0] if isinstance(x, str) else np.nan)
-    return(df[['proteinId', 'SP', 'SP_cs']])
+    return df[['proteinId', 'SignalP6', 'SP_cs']]
--- src/parse_localization.py
+++ src/parse_localization.py
@@ -1,8 +1,9 @@
+import os
+import sys
 import pandas as pd
 import numpy as np
 
-import os
-import sys
+
 
 from src.localization.deeptmhmm import deeptmhmm
 from src.localization.signalp6 import signalp6
@@ -11,15 +12,14 @@ from src.localization.deeploc2 import deeploc2
 
 def location( signalp_output_file, deeptmhmm_output_file, deeploc2_output_file, output_file_path):
     """
-    Give outputs of SignalP6, DeepTMHMM and DeepLoc2. This function will classify these proteins with the following operations:
-    Extracellular: DeepLoc2 | (SignalP6 & no TM ) 
-    Transmembrane: DeepTMHMM
-    Intracellular: All the rest
-    
-    Keep in mind 
+    Give outputs of SignalP6, DeepTMHMM and DeepLoc2. 
+    This function will classify these proteins with the following operations:
+        Extracellular: DeepLoc2 | (SignalP6 & no TM ) 
+        Transmembrane: DeepTMHMM
+        Intracellular: All the rest
     
     Returns the dataframe and saves it in output_file_path
-    """  
+    """
     # parse the outputs
     signalp_data = signalp6(signalp_output_file)
     deeptmhmm_data = deeptmhmm(deeptmhmm_output_file)
@@ -27,7 +27,7 @@ def location( signalp_output_file, deeptmhmm_output_file, deeploc2_output_file, 
 
     df = pd.merge(deeploc2_data[['proteinId', 'DeepLoc2', 'Signals_DeepLoc2']], signalp_data, on = 'proteinId', how='outer')
     df = pd.merge(df, deeptmhmm_data, on = 'proteinId', how='outer')
-    
+
     # if you do not run DeepTMHMM for all proteins then there will be NaN values in these columns
     tm_vals = [s for s in df.loc[~df['DeepTMHMM'].isnull()]['DeepTMHMM'].unique() if "TMhelix" in s]
     periplasm_vals = [s for s in df.loc[~df['DeepTMHMM'].isnull()]['DeepTMHMM'].unique() if "periplasm" in s]
@@ -35,7 +35,7 @@ def location( signalp_output_file, deeptmhmm_output_file, deeploc2_output_file, 
     
     # categorize
     df.loc[df['DeepLoc2'].str.contains("Extracellular"), 'Location'] = "Extracellular"
-    df.loc[ (df['SP'] == 'Y') & (~df['DeepTMHMM'].isin(tm_vals + periplasm_vals)), 'Location'] = 'Extracellular'
+    df.loc[ (df['SignalP6'] == 'SP') & (~df['DeepTMHMM'].isin(tm_vals + periplasm_vals)), 'Location'] = 'Extracellular'
     
     df.loc[df['DeepTMHMM'].isin(tm_vals), 'Location'] = 'Transmembrane'
 
@@ -43,4 +43,4 @@ def location( signalp_output_file, deeptmhmm_output_file, deeploc2_output_file, 
 
     df.to_csv(output_file_path, index=False)
     
-    return(df)
+    return df
