43a01050da9b072b6c25f34bae9ad31812004949
Roman-Si <roman_siap@hotmail.com>
2025-05-13 15:17:13 +0300

    Localization annotation in one function now

--- src/protannoscripts/parse_localization.py
+++ src/protannoscripts/parse_localization.py
@@ -1,42 +1,67 @@
 import os
-import sys
 import pandas as pd
-import numpy as np
 
-from .deeptmhmm import deeptmhmm    
-from .signalp6 import signalp6
-from .deeploc2 import deeploc2
+from .signalp6 import signalp6_to_df
+from .deeploc2 import deeploc2_to_df
+from .tmhmm import tmhmm_to_df
+from .netgpi import netgpi_to_df
 
 
-def location( signalp_output_file: str, deeptmhmm_output_file: str, deeploc2_output_file: str) -> pd.DataFrame:
+def annotate_location(
+        unifire_df = None, 
+        functional_info_dir = "data/functional/location/",  
+        signalp_output_file = "signalp6/prediction_results.txt.gz", tmhmm_sp_output_file = "signalp6/tmhmm2.out.gz", netgpi_output_file = "signalp6/netgpi.out.gz",
+        tmhmm_output_file = "tmhmm2/tmhmm2.out.gz",
+        deeploc2_output_file = "deeploc2/deeploc2.csv.gz", deeploc_extracellular_threshold = 0.52753906, deeploc_membrane_threshold = 0.64638672) -> pd.DataFrame:
     """
-    Give outputs of SignalP6, DeepTMHMM and DeepLoc2. 
-    This function will classify these proteins with the following operations:
-        Extracellular: DeepLoc2 | (SignalP6 & no TM ) 
-        Transmembrane: DeepTMHMM
-        Intracellular: All the rest
+    Give the path to the localization annotations directory, the relative path of each output file to this directory and a dataframe with the parsed UniFire annotations with columns ['proteinId', 'unifire_loc'] if available. 
+    This function will classify these proteins with the following order:
+    1. SignalP6 && NetGPI ->  Extracellular GPI-anchored
+    2. DeepLoc2 Extracellular column > 0.52753906 -> Extracellular
+    3. unifire_loc -> Intracellular, Extracellular, Membrane
+    4. SignalP6 && noTM -> Extracellular
+    5. TMHMM | DeepLoc2 -> Membrane
+    6. Deeploc(Nucles|Cytoplasm) -> Intracellular
+    7. Rest remains blank but is probably Intracellular
     
-    Returns the dataframe
+    Returns the dataframe with assigned annotation in 'Location' column but contains all the info as well.
     """
-    # parse the outputs
-    signalp_data = signalp6(signalp_output_file)
-    deeptmhmm_data = deeptmhmm(deeptmhmm_output_file)
-    deeploc2_data = deeploc2(deeploc2_output_file)
 
-    df = pd.merge(deeploc2_data[['proteinId', 'DeepLoc2', 'Signals_DeepLoc2']], signalp_data, on = 'proteinId', how='outer')
-    df = pd.merge(df, deeptmhmm_data, on = 'proteinId', how='outer')
+    ### Load data
+    signalp_df = signalp6_to_df(os.path.join(functional_info_dir + signalp_output_file))
+    netgpi_df = netgpi_to_df(os.path.join(functional_info_dir + netgpi_output_file))
+    deeploc_df = deeploc2_to_df(os.path.join(functional_info_dir + deeploc2_output_file))
+    # For proteins with signal peptide (SP) replace the initial tmhmm annotation with that of the SP-stripped sequence 
+    tmhmm_df = tmhmm_to_df(os.path.join(functional_info_dir + tmhmm_output_file)).set_index('proteinId')
+    tmhmm_sp_df = tmhmm_to_df(os.path.join(functional_info_dir + tmhmm_sp_output_file)).set_index('proteinId')
+    tmhmm_df = tmhmm_sp_df.combine_first(tmhmm_df)
+    tmhmm_df = tmhmm_df.reset_index()
 
-    # if you do not run DeepTMHMM for all proteins then there will be NaN values in these columns
-    tm_vals = [s for s in df.loc[~df['DeepTMHMM'].isnull()]['DeepTMHMM'].unique() if "TMhelix" in s]
-    periplasm_vals = [s for s in df.loc[~df['DeepTMHMM'].isnull()]['DeepTMHMM'].unique() if "periplasm" in s]
-    
-    
-    # categorize
-    df.loc[df['DeepLoc2'].str.contains("Extracellular"), 'Location'] = "Extracellular"
-    df.loc[ (df['SignalP6'] == 'SP') & (~df['DeepTMHMM'].isin(tm_vals + periplasm_vals)), 'Location'] = 'Extracellular'
-    
-    df.loc[df['DeepTMHMM'].isin(tm_vals), 'Location'] = 'Transmembrane'
+    ### Merge data
+    # If dataframe with unifire_loc is not available will start with the deeploc2 output
+    if unifire_df is not None:
+        location_df = unifire_df[['proteinId', 'unifire_loc']]
+    else:
+        location_df = deeploc_df[['proteinId']].copy()
+        location_df['unifire_loc'] = pd.NA
+        
+    for dataframe in [signalp_df, netgpi_df, tmhmm_df[['proteinId', 'ExpAA', 'PredHel', 'Topology']], deeploc_df]:
+        location_df = location_df.merge(dataframe, on = "proteinId", how = "left")
 
-    df['Location'] = df['Location'].fillna('Intracellular')
+    ### Annotate with the rules
+    # 1. NetGPI after SignalP6 -> Extracellular;GPI
+    location_df.loc[(location_df["NetGPI"] == "Y") , "Location"] = "Extracellular;GPI"
+    # 2. Filter for deeploc_extracellular_threhold and select those that start with Extracellular
+    location_df.loc[(location_df['Extracellular'] > deeploc_extracellular_threshold) & ( location_df['DeepLoc2'].str.startswith("Extracellular") ) & (location_df['Location'].isnull()), 'Location'] = location_df['DeepLoc2']
+    # 3. UniFire
+    if unifire_df is not None:
+        location_df.loc[location_df['Location'].isnull(), 'Location']  = location_df["unifire_loc"]
+    # 4. SignalP & no TMhelix -> Extracellular
+    location_df.loc[(location_df['SignalP6'] == 'SP') & (location_df['PredHel'] == 0) & (location_df['Location'].isnull()), 'Location'] = 'Extracellular'
+    # 5. TMHMM | DeepLoc2 for membrane proteins
+    location_df.loc[(location_df['Location'].isnull()) & (location_df['Cell membrane'] > deeploc_membrane_threshold) & ( location_df['DeepLoc2'].str.startswith("Cell membrane") ), 'Location'] = 'Membrane'
+    location_df.loc[(location_df['Location'].isnull()) & (location_df['PredHel'] > 0) , 'Location'] = 'Membrane'
+    # 6. Intracellular: Proteins without any other annotation and DeepLoc2 starts with Cytoplasm | Nucleus
+    location_df.loc[ (location_df['Location'].isnull()) & ( ( location_df['DeepLoc2'].str.startswith("Cytoplasm") ) | ( location_df['DeepLoc2'].str.startswith("Nucleus") ) ), "Location"] = "Intracellular"
     
-    return df
+    return location_df
